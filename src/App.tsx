762a73935eb8fee674694778bd98e274de2d2e67
Rodrigo Almeida <rodrigoalmeida@example.com>
2026-05-12 15:08:10 -0300

    alteração refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import HomePage from './pages/HomePage'
 import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
 import { CMSApp } from './cms/CMSApp'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  
+  return null
+}
+
 function App() {
+  useEffect(() => {
+    // Força a página a carregar no topo no F5/Refresh
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+    }
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/:slug" element={<BlogPost />} />
-      <Route path="/cms" element={<CMSApp />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/cms" element={<CMSApp />} />
+      </Routes>
+    </>
   )
 }
 
